a5a39374596864566437e75307b33a79e56f15cb
Nik Furlan <nik@furlanov.com>
2023-06-15 19:18:21 +0200

    Improvments

--- app.py
+++ app.py
@@ -1,43 +1,76 @@
-from flask import Flask, request, send_file, render_template
+from flask import Flask, request, send_file, render_template, abort
 from PIL import Image
 import datetime
 import zipfile
+import requests
 import secrets
+import openai
 import io
 import os
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
 
+
 @app.route('/')
 def index():
     return render_template('index.html')
 
-def get_renamed_image_filename(image, img, output_format, rename_format):
-    original_name, original_ext = os.path.splitext(image.filename)
-    if rename_format == 'add_resolution':
-        width, height = img.size
-        return f'{original_name}_{width}x{height}.{output_format}'
-    if rename_format == 'add_date':
-        current_date = datetime.date.today()
-        return f'{original_name}_{current_date.strftime("%d_%m_%Y")}.{output_format}'
-    else:
-        return f'{original_name}.{output_format}'
 
 @app.route('/resize_images', methods=['POST'])
 def resize_images():
+    resize_type = request.form['resize-type']
+    if resize_type == 'ai':
+        return resize_ai_image()
+    else:
+        return resize_simple_images()
+
+
+def resize_ai_image():
+    openai.api_key = ""
+    image = request.files['images']
+    allowed_file(image.filename)
+
+    img = Image.open(image)
+    img = resize_with_crop(img, 512, 512)
+    canvas = Image.new('RGBA', (1024, 1024), (0, 0, 0, 0))
+    x = (canvas.width - img.width) // 2
+    y = (canvas.height - img.height) // 2
+    canvas.paste(img, (x, y))
+
+    temp_filename = os.path.join(os.path.dirname(__file__), "temp", f"{secrets.token_hex(8)}.png")
+    canvas.save(temp_filename)
+
+    response = openai.Image.create_edit(
+        image=open(temp_filename, "rb"),
+        mask=open(temp_filename, "rb"),
+        prompt="Fill the transparent space",
+        n=1,
+        size="1048x1048"
+    )
+    image_url = response['data'][0]['url']
+
+    response = requests.get(image_url, stream=True)
+    response.raise_for_status()
+
+    image_io = io.BytesIO(response.content)
+
+    original_name, original_ext = os.path.splitext(image.filename)
+    output_filename = f'{original_name}.png'
+
+    return send_file(image_io, mimetype='image/png', as_attachment=True, download_name=output_filename)
+
+
+def resize_simple_images():
     height = int(request.form['height']) if request.form['height'] else None
     aspect_ratio = request.form.get('aspect-ratio') == 'on'
     images = request.files.getlist('images')
     output_format = request.form['output-format']
     rename_format = request.form['rename-format']
     resized_images = []
-
     for image in images:
-        if not image:
-            return "No file selected"
-        if not allowed_file(image.filename):
-            return "Invalid file type. Images with extensions - JPG, JPEG, PNG, ICO, and GIF are allowed"
+        allowed_file(image.filename)
+
         width = int(request.form['width']) if request.form.get('width') and not aspect_ratio else None
         image_io = io.BytesIO(image.read())
         img = Image.open(image_io)
@@ -68,20 +101,19 @@ def resize_images():
         elif output_format == 'webp':
             img_format = 'WEBP'
         else:
-            return "Invalid output format. Choose between JPG, JPEG, PNG, ICO and GIF."
-        
+            abort(400, "Invalid output format. Choose between JPG, JPEG, PNG, ICO, and GIF.")
+
         if img.mode == 'RGBA' and img_save_format:
             img = img.convert(img_save_format)
-            
+
         img.save(image_io, img_format)
         image_io.seek(0)
         resized_images.append((image_io, get_renamed_image_filename(image, img, output_format, rename_format)))
 
-    zip_file = io.BytesIO()
-
     if len(resized_images) == 1:
         return send_file(resized_images[0][0], download_name=resized_images[0][1], as_attachment=True)
     else:
+        zip_file = io.BytesIO()
         with zipfile.ZipFile(zip_file, mode='w') as zf:
             for i, (resized_image, image_name) in enumerate(resized_images):
                 zf.writestr(image_name, resized_image.getbuffer())
@@ -89,10 +121,51 @@ def resize_images():
         zip_file.seek(0)
         return send_file(zip_file, download_name='resized_images.zip', as_attachment=True)
 
+
 def allowed_file(filename):
     ALLOWED_EXTENSIONS = {'jpg', 'jpeg', 'png', 'gif', 'ico', 'webp'}
-    return '.' in filename and \
-        filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+    if not '.' in filename or \
+            filename.rsplit('.', 1)[1].lower() not in ALLOWED_EXTENSIONS:
+        abort(400, "Invalid file type. Images with extensions - JPG, JPEG, PNG, ICO, and GIF are allowed")
+
+
+def get_renamed_image_filename(image, img, output_format, rename_format):
+    original_name, original_ext = os.path.splitext(image.filename)
+    if rename_format == 'add_resolution':
+        width, height = img.size
+        return f'{original_name}_{width}x{height}.{output_format}'
+    if rename_format == 'add_date':
+        current_date = datetime.date.today()
+        return f'{original_name}_{current_date.strftime("%d_%m_%Y")}.{output_format}'
+    else:
+        return f'{original_name}.{output_format}'
+
+
+def resize_with_crop(image, target_width, target_height):
+    width, height = image.size
+    aspect_ratio = width / height
+
+    target_aspect_ratio = target_width / target_height
+
+    if aspect_ratio > target_aspect_ratio:
+        new_width = int(height * target_aspect_ratio)
+        left = (width - new_width) // 2
+        top = 0
+        right = left + new_width
+        bottom = height
+    else:
+        new_height = int(width / target_aspect_ratio)
+        left = 0
+        top = (height - new_height) // 2
+        right = width
+        bottom = top + new_height
+
+    image = image.crop((left, top, right, bottom))
+
+    image = image.resize((target_width, target_height))
+
+    return image
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0')
--- static/scripts.js
+++ static/scripts.js
@@ -10,3 +10,32 @@ function toggleAspectRatio() {
         widthInput.setAttribute('placeholder', 'Width (px)');
     }
 }
+
+function toggleResizeType() {
+    const resizeType = document.getElementById('resize-type').value;
+    const heightInput = document.getElementById('height');
+    const widthInput = document.getElementById('width');
+    const fileInput = document.getElementById('images');
+
+    if (resizeType === 'ai') {
+        heightInput.value = 1024;
+        widthInput.value = 1024;
+        heightInput.setAttribute('readonly', 'readonly');
+        widthInput.setAttribute('readonly', 'readonly');
+        heightInput.classList.add('grayed-out');
+        widthInput.classList.add('grayed-out');
+        document.getElementById('aspect-ratio').setAttribute('disabled', 'disabled');
+        fileInput.setAttribute('required', true);
+        fileInput.removeAttribute('multiple');
+    } else {
+        heightInput.value = '';
+        widthInput.value = '';
+        heightInput.removeAttribute('readonly');
+        widthInput.removeAttribute('readonly');
+        heightInput.classList.remove('grayed-out');
+        widthInput.classList.remove('grayed-out');
+        document.getElementById('aspect-ratio').removeAttribute('disabled');
+        fileInput.setAttribute('multiple', false);
+        fileInput.setAttribute('required', true);
+    }
+}
